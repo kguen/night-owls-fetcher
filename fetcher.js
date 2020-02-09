d0b03ed386cfdb6550aee7d4a7bafe5f42f9e05e
Khoa <nanhkhoa460@gmail.com>
2020-02-10 00:17:32 +0700

    fetch details + filter by tag

--- fetcher.js
+++ fetcher.js
@@ -14,22 +14,6 @@ const getGenres = async (format) => {
   return data.genres;
 }
 
-const getOMDbRatings = async (IMDbId) => {
-  const OMDbData = await fetch(`${OMDbUrl}/?apiKey=${OMDbApiKey}&i=${IMDbId}`)
-    .then(res => res.json());
-  let rt_score;
-  if (OMDbData.Ratings && OMDbData.Ratings.length) {
-    rt_score = OMDbData.Ratings.find(item => item.Source === 'Rotten Tomatoes');
-    rt_score = rt_score && rt_score.Value;
-  }
-  return { 
-    imdb_rating: OMDbData.imdbRating,
-    imdb_votes: OMDbData.imdbVotes,
-    meta_score: OMDbData.Metascore,
-    rt_score
-  };
-}
-
 const getTMDbSearchData = async (format, query, page) => {
   let params = `&page=${page}&query=${query}`;
   let data = await fetch(`${TMDbUrl}/search/${format}?api_key=${TMDbApiKey}${params}`)
@@ -43,7 +27,7 @@ const getTMDbTrendingData = async (format, page) => {
   return { totalResults: data.total_results, data: data.results };
 }
 
-const getTMDbNormalData = async (format, genres, rating, fromYear, toYear, page, tab) => {
+const getTMDbNormalData = async (format, genres, rating, fromYear, toYear, page, withQuery, tab) => {
   let params = `&page=${page}`;
   if (genres && genres.length) {
     params += `&with_genres=${genres.join(',')}`;
@@ -61,33 +45,81 @@ const getTMDbNormalData = async (format, genres, rating, fromYear, toYear, page,
       ? `&primary_release_date.lte=${toYear}-12-31`
       : `&air_date.lte=${toYear}-12-31`;
   }
-  let data = await fetch(`${TMDbUrl}/${format}/${tab}?api_key=${TMDbApiKey}${params}`)
+  if (withQuery) {
+    params += `&${withQuery}`;
+  }
+  const data = await fetch(`${TMDbUrl}/${format}/${tab}?api_key=${TMDbApiKey}${params}`)
     .then(res => res.json());
   return { totalResults: data.total_results, data: data.results };
+}
+
+const getOMDbRatings = async (IMDbId) => {
+  const OMDbData = await fetch(`${OMDbUrl}/?apiKey=${OMDbApiKey}&i=${IMDbId}`)
+    .then(res => res.json());
+  let rt_score;
+  if (OMDbData.Ratings && OMDbData.Ratings.length) {
+    rt_score = OMDbData.Ratings.find(item => item.Source === 'Rotten Tomatoes');
+    rt_score = rt_score && rt_score.Value;
+  }
+  return { 
+    imdb_rating: OMDbData.imdbRating,
+    imdb_votes: OMDbData.imdbVotes,
+    meta_score: OMDbData.Metascore,
+    rt_score
+  };
+}
+
+const getAddiionalDetails = async (format, item) => {
+  const externalIds = await fetch(`${TMDbUrl}/${format}/${item.id}/external_ids?api_key=${TMDbApiKey}`)
+    .then(res => res.json());
+  item = {...item, ...externalIds};
+  if (item.poster_path) {
+    const poster_path = `${TMDbImageURL}${item.poster_path}`;
+    item = {...item, poster_path};
+  }
+  if (item.imdb_id) {
+    const OMDbRatings = await getOMDbRatings(item.imdb_id);
+    item = {...item, ...OMDbRatings};
+  }
+  return {...item, format};
+}
+
+const getFullData = async (format, genres, rating, fromYear, toYear, page, withQuery, searchQuery, tab) => {
+  if (searchQuery !== '' && searchQuery !== undefined) {
+    TMDbResponse = await getTMDbSearchData(format, searchQuery, page);
+  } else if (tab !== 'trending') {
+    TMDbResponse = await getTMDbNormalData(format, genres, rating, fromYear, toYear, page, withQuery, tab);
+  } else {
+    TMDbResponse = await getTMDbTrendingData(format, page);
+  }
+  let fetchedData = await Promise.all(
+    TMDbResponse.data.map(async item => await getAddiionalDetails(format, item))
+  );
+  return { data: fetchedData, total_results: TMDbResponse.totalResults };
 }
 
 const getTMDbDetails = async (format, id) => {
   const details = await fetch(`${TMDbUrl}/${format}/${id}?api_key=${TMDbApiKey}`)
     .then(res => res.json());
-  const externalIds = await fetch(`${TMDbUrl}/${format}/${id}/external_ids?api_key=${TMDbApiKey}`)
-    .then(res => res.json());
   const credits = await fetch(`${TMDbUrl}/${format}/${id}/credits?api_key=${TMDbApiKey}`)
     .then(res => res.json());
   const keywords = await fetch(`${TMDbUrl}/${format}/${id}/keywords?api_key=${TMDbApiKey}`)
-    .then(res => res.json());
-  const recommendations = await fetch(`${TMDbUrl}/${format}/${id}/recommendations?api_key=${TMDbApiKey}`)
     .then(res => res.json());
   const videos = await fetch(`${TMDbUrl}/${format}/${id}/videos?api_key=${TMDbApiKey}`)
     .then(res => res.json());
   const casts = credits.cast.slice(0, 10),
     video = videos.results.find(item => item.site === 'YouTube' && item.type === 'Trailer');
-  const ratings = externalIds.imdb_id && await getOMDbRatings(externalIds.imdb_id);  
+  let recommendations = await fetch(`${TMDbUrl}/${format}/${id}/recommendations?api_key=${TMDbApiKey}`)
+    .then(res => res.json());
+  recommendations = recommendations.results.length 
+    ? await Promise.all( 
+      recommendations.results.map(async item => await getAddiionalDetails(format, item))
+    ) : [];
   const baseInfo = {
-    ...details, ...externalIds, ...ratings,
-    casts, video, 
+    ...await getAddiionalDetails(format, details), 
+    casts, video, recommendations,
     keywords: format === 'movie' ? keywords.keywords : keywords.results,
-    recommendations: recommendations.results,
-  }
+  };
   if (format === 'movie') {
     const directors = credits.crew.filter(item => item.job === 'Director'),
       writers = credits.crew.filter(item => item.department === 'Writing')
@@ -98,32 +130,10 @@ const getTMDbDetails = async (format, id) => {
   }
 }
 
-const getFullData = async (format, genres, rating, fromYear, toYear, page, query, tab) => {
-  if (query !== '' && query !== undefined) {
-    TMDbResponse = await getTMDbSearchData(format, query, page);
-  } else if (tab !== 'trending') {
-    TMDbResponse = await getTMDbNormalData(format, genres, rating, fromYear, toYear, page, tab);
-  } else {
-    TMDbResponse = await getTMDbTrendingData(format, page);
-  }
-  let fetchedData = await Promise.all(
-    TMDbResponse.data.map(async item => {
-      const TMDbId = item.id;
-      let externalIds = await fetch(`${TMDbUrl}/${format}/${TMDbId}/external_ids?api_key=${TMDbApiKey}`)
-        .then(res => res.json());
-      item = {...item, ...externalIds};
-      if (item.poster_path) {
-        const poster_path = `${TMDbImageURL}${item.poster_path}`;
-        item = {...item, poster_path};
-      }
-      if (item.imdb_id) {
-        const OMDbRatings = await getOMDbRatings(item.imdb_id);
-        item = {...item, ...OMDbRatings};
-      }
-      return {...item, format};
-    })
-  );
-  return { data: fetchedData, total_results: TMDbResponse.totalResults };
+const getOtherDetails = async (format, id) => {
+  const data = await fetch(`${TMDbUrl}/${format}/${id}?api_key=${TMDbApiKey}`)
+    .then(res => res.json());
+  return data;
 }
 
-module.exports = { getFullData, getGenres, getTMDbDetails }
+module.exports = { getFullData, getGenres, getTMDbDetails, getOtherDetails }
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const { getFullData, getGenres, getTMDbDetails } = require('./fetcher.js');
+const { getFullData, getGenres, getTMDbDetails, getOtherDetails } = require('./fetcher.js');
 
 app.get('/api/movies', async (req, res) => {
-  const { format, genres, rating, fromYear, toYear, page, query, tab } = req.query;
-  const data = await getFullData(format, genres, rating, fromYear, toYear, page, query, tab);
+  const { format, genres, rating, fromYear, toYear, page, withQuery, searchQuery, tab } = req.query;
+  const data = await getFullData(format, genres, rating, fromYear, toYear, page, withQuery, searchQuery, tab);
   res.send(data);
 });
 
@@ -17,6 +17,11 @@ app.get('/api/details', async (req, res) => {
   const data = await getTMDbDetails(req.query.format, req.query.id);
   res.send(data);
 })
+
+app.get('/api/others', async (req, res) => {
+  const data = await getOtherDetails(req.query.format, req.query.id);
+  res.send(data);
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
